4acc27408fa8951bd1f70216be690c54cb299fc0
Shubham Saxena <shubh.lpu16@gmail.com>
2023-12-25 21:35:19 +0530

    add: color tool

---
+++ color/app.js
@@ -0,0 +1,5 @@
+function colorChange() {
+  const container = document.getElementById("container");
+  const select = document.getElementById("colors");
+  container.style.backgroundColor = select.value;
+}
